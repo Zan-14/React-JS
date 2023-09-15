bc5670d6f0d7caab2e09c1a59b8e1c8b4c558ba5
Zan-14 <fauzanjumdinharm@gmail.com>
2023-09-15 16:53:26 +0700

    learn about components

---
+++ src/components/header/header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <div className="bg-yellow-600 p-5"></div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/header/nav-bar.jsx
@@ -0,0 +1,12 @@
+function NavBar() {
+  return (
+    <nav>
+      <div className="flex items-center justify-between">
+        <div></div>
+        <div></div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
