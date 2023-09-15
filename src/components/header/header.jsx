b2693542ad9b80c7c8a16ff1ba11fd4099f37cd4
Zan-14 <fauzanjumdinharm@gmail.com>
2023-09-15 17:44:05 +0700

    new branch

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,11 @@
 import NavBar from "./nav-bar";
+// don't forget to import the child components into the parent component
 
 function Header() {
   return (
     <header>
       <div className="bg-yellow-600 p-5">
+        {/* this is how to import the component */}
         <NavBar />
       </div>
     </header>
