8a4b3c16f925d358e89b2b137a12c72f7f527699
Zan-14 <fauzanjumdinharm@gmail.com>
2023-09-16 20:27:03 +0700

    progress to create the game history

--- ReadMe-Project1.txt
+++ ReadMe-Project1.txt
@@ -4,13 +4,43 @@ Main materials/Tutorial:
 1. Official React Website:
 react.dev/learn/tutorial-tic-tac-toe
 
-2. youtube:
+2. youtube: (48:00)
 https://www.youtube.com/watch?v=RVH_5L5Lsp0&list=PLFIM0718LjIUu3X2zYNqomEWs3sYd-fV1&index=2
 
 Step by step in general:
 0. Preparation
+    - React + Vite + Tailwind installation and configuration
 1. Creating the game board
+    a. Create a component of a square
+    b. Import the square component into the board component (9x)
+    c. Create a state in the square component when button is clicked, X letter will fill the square.
 2. Adding the other player
+    - using Lifting state up concept
+    explanation: We need to make the board component know about the state or condition of each square, whether it is filled or not. Because by default, the board doesn't know about the state or condition of the child components.
+    - Before, the square will be filled with X only. We need to create a turn for second player with O letter to fill the square.
 3. Determine the winner
 4. Adding "time travel" feature (game timeline / history)
 5. Refactor the code
+
+
+*Lesson learned from this PROJECT
+1. Lifting state up concept
+2. Immutability state (important)
+    Benefits:
+    - Clone an existing data and modified it without changing the original data
+    - Immutability avoids the need to re render all of the child components inside a parent component when there is a data change, even though some data inside the child component isn't change. This will increase the performance of the App.
+
+
+
+// const App = () => {
+//   return (
+//     <>
+//       {/* template header */}
+//       <h1 className="bg-black text-white text-3xl text-center">
+//         Learn React JS (Tic Tac Toe Game)
+//       </h1>
+//     </>
+//   );
+// };
+
+// export default App;
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,86 @@
-// the other way to type a function. So below is similar to function App() {}
-const App = () => {
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+// 3. adding X after click on the board
+function Square({ value, onSquareClick }) {
+  return (
+    // 1. individual block on the board
+    <button
+      onClick={onSquareClick}
+      className="w-16 h-16 bg-black border-4 border-white text-3xl font-bold text-white"
+    >
+      {value}
+    </button>
+  );
+}
+
+// Winner rules
+function determineWinner(squares) {
+  const rules = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < rules.length; i++) {
+    const [a, b, c] = rules[i];
+
+    if (squares[a] == squares[b] && squares[b] == squares[c]) {
+      return squares[a];
+    }
+  }
+  return false;
+}
+
+// Board component
+const Board = () => {
+  // 4. create a state with an array of 9 and the default values is null
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  // create a taking turn between player x and player o
+  const [xIsNext, setXIsNext] = useState(true);
+  function afterClick(i) {
+    // Prevent the filled square to change the value
+    if (squares[i] || determineWinner(squares)) return;
+    // clone the array data from no.4 or const square
+    const nextSquares = squares.slice();
+    // Use ternary operator to replace the if statement and create a turn
+    nextSquares[i] = xIsNext ? "X" : "O";
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  const winner = determineWinner(squares);
+  let status = "";
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ` + (xIsNext ? "X" : "O");
+  }
+
   return (
     <>
-      <div>
-        <div>
-          <h1 className="bg-black text-white text-3xl text-center">
-            Learn React JS
-          </h1>
-          <h1 className="bg-yellow-500 text-white text-3xl text-center py-2">
-            Installation and Configuration
-          </h1>
+      {/* tic-tac-toe */}
+      <div className="flex flex-col place-items-center mt-10">
+        {/* Game Condition */}
+        <div className="text-xl font-bold">{status}</div>
+        {/* 2. Board */}
+        <div className="flex flex-wrap w-48 h-48 mt-4">
+          <Square value={squares[0]} onSquareClick={() => afterClick(0)} />
+          <Square value={squares[1]} onSquareClick={() => afterClick(1)} />
+          <Square value={squares[2]} onSquareClick={() => afterClick(2)} />
+          <Square value={squares[3]} onSquareClick={() => afterClick(3)} />
+          <Square value={squares[4]} onSquareClick={() => afterClick(4)} />
+          <Square value={squares[5]} onSquareClick={() => afterClick(5)} />
+          <Square value={squares[6]} onSquareClick={() => afterClick(6)} />
+          <Square value={squares[7]} onSquareClick={() => afterClick(7)} />
+          <Square value={squares[8]} onSquareClick={() => afterClick(8)} />
         </div>
       </div>
     </>
   );
 };
-
-export default App;
+export default Board;
